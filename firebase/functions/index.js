d5efa9296a9fb27037a8e6f30786ac0d8ea5cd86
YongZhen Low <s4.yz@wearesection.com>
2020-04-24 21:28:28 +0800

    ci(firebase/functions): add nuxt middleware

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,18 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const { Nuxt } = require('nuxt-start')
+const nuxtConfig = require('./nuxt-serverless/nuxt.config.js')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const nuxt = new Nuxt({
+  ...nuxtConfig,
+  dev: false, // required for `nuxt-start`
+  buildDir: './nuxt-serverless/.nuxt' // point to the built dir that was copied over in predeploy script
+})
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+exports.nuxtServerless = functions.https.onRequest(
+  async (request, response) => {
+    await nuxt.ready()
+    nuxt.render(request, response)
+  }
+)
